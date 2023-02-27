e313ddde3e98edd20835da4b014123c63404098e
Fredy Polania <fpolania@gmail.com>
2023-02-27 15:23:48 -0400

    add: page components

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,7 +2,7 @@ export default {
 	title: `Seafood Depot LLC`,
 	description: `Seafood Depot web app`,
 	url: `http://seafooddepotllc.com/`,
-	name: `Seafood`,
+	name: `Seafood Depot`,
 	address: `7520 NW 104th Ave Ste A103 PMB 4222 Doral, FL 33178-3374`,
 	analytics: "" // analytics code
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require("@tailwindcss/typography"),
-		require('daisyui')
-	],
-	darkMode: 'class',
-	// daisyUI config (optional)
-	daisyui: {
-		styled: true,
-		themes: ["garden", "dark"],
-		base: true,
-		utils: true,
-		logs: false,
-		rtl: false,
-		prefix: "",
-		darkTheme: "dark",
-	},
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  darkMode: "class",
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        seafood: {
+          primary: "#1c2a35",
+          secondary: "#24221c",
+          accent: "#3d3d3d",
+          neutral: "#999999",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+    base: true,
+    utils: true,
+    logs: false,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+};
