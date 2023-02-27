6174a3a45ead60d7a6bca556e31b9e4574c85b02
Fredy Polanía <fpolania@gmail.com>
2023-02-27 18:05:34 -0400

    fix: site URL

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Seafood Depot LLC`,
 	description: `Seafood Depot web app`,
-	url: `http://seafooddepotllc.com/`,
+	url: `https://seafooddepotllc.com/`,
 	name: `Seafood Depot`,
 	address: `7520 NW 104th Ave Ste A103 PMB 4222 Doral, FL 33178-3374`,
 	analytics: "" // analytics code
